a39b2113132449b5265c86e315466745f109171f
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-06-23 13:53:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,16 +4,25 @@ const delayInput=document.getElementById('delay')
 const submitButton=document.getElementById('btn')
 const outputDiv=document.getElementById('output')
  
- 
-const timeout=(delay)=>{
+const solve = ()=>{
 	return new Promise((res,rej)=>{
-		setTimeout(()=>res(),delay)
+		setTimeout(()=>{
+			outputDiv.textContent=textInput.value
+			res()
+		},delayInput.value)
 	})
 }
 
-const solve = async ()=>{
-	await timeout(delayInput.value)
-	outputDiv.textContent=textInput.value
-}
+
+// const timeout=(delay)=>{
+// 	return new Promise((res,rej)=>{
+// 		setTimeout(()=>res(),delay)
+// 	})
+// }
+
+// const solve = async ()=>{
+// 	await timeout(delayInput.value)
+// 	outputDiv.textContent=textInput.value
+// }
 
 submitButton.addEventListener('click',solve)
